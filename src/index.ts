1d8f5e818383cb878a51ae09a37fbc68cd773558
ascoders <576625322@qq.com>
2017-10-14 22:38:54 +0800

    add translate

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as translate from './utils/translate'
+
 export { default as GameControl } from './game-control'
 export { default as GameObject } from './game-object'
 export { default as Timer } from './utils/timer'
+export { translate }
